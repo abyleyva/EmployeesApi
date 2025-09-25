c39cfd4751f0177c8fbae74d93a75b1a0a0bab6b
Aby Leyva <abyleyva75@gmail.com>
2025-09-25 17:07:27 -0600

    add endpoints get employees and getemployees by id

--- Program.cs
+++ Program.cs
@@ -3,9 +3,33 @@ using EmployeesApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-var Employees = new List<Employee>();
+var employees = new List<Employee>();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/employees", () => employees);
+
+app.MapGet("/employees/{id}", (int id) =>
+employees.FirstOrDefault(e => e.Id == id) is Employee e ? Results.Ok(e) : Results.NotFound()
+/* 
+same as above
+{
+    var employee = employees.FirstOrDefault(x => x.Id == id);
+    if (employee is  not null)
+    {
+        return Results.Ok(employee);
+    }
+    return Results.NotFound();
+} */
+);
+
+app.MapPost("/employees", (Employee employee) =>
+{
+    employee.Id = employees.Count > 0 ? employees.Max(e => e.Id) + 1 : 1;
+    employees.Add(employee);
+    return Results.Created($"/employees/{employee.Id}", employee);
+}
+);
+
+
 
 
 
